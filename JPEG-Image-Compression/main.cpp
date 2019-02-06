675a4d7e321996a8d980e56010a9d35ad979e3bd
Maxim Puchkov <mpuchkov@sfu.ca>
2019-02-06 15:00:27 -0800

    Initial Commit

---
+++ JPEG-Image-Compression/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  JPEG-Image-Compression
+//
+//  Created by admin on 2019-02-06.
+//  Copyright © 2019 maximpuchkov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
