1b12fb369d4376ea14ccb8f73a00d97eb898188f
Maxim Puchkov <mpuchkov@sfu.ca>
2019-02-22 22:44:31 -0800

    Partition 8×8 blocks

--- include/Compression.h
+++ include/Compression.h
@@ -87,6 +87,8 @@ Mat3b rgbImage(int rows, int cols) {
 
 /** Compression **/
 
+const int BLOCK_DIMENSION = 8;
+
 // JPEG compression of source image
 /* Mat3b */ void compress(const Mat3b &source) {
     Mat3b compressed = source.clone();
@@ -96,8 +98,14 @@ Mat3b rgbImage(int rows, int cols) {
     // 2. chroma subsampling 4:2:0 (CV_8SC3)
     
     // 3. Partition image into 8×8 blocks (CV_8SC3)
-    for (int row = 0; row < source.rows; row++) {
-        for (int col = 0; col < source.cols; col++) {
+    const int N = BLOCK_DIMENSION;
+    int rowLimit = (source.rows % N) * N;
+    int colLimit = (source.cols % N) * N;
+    
+    for (int row = 0; row < rowLimit; row += N) {
+        for (int col = 0; col < colLimit; col += N) {
+            
+            Mat block(N, N, CV_8SC3);
             
             // 4. DCT transformation of each image block (CV_8SC3)
             
