329c54b4e2760529c7b92b4d7bb957004c2703cf
Maxim Puchkov <mpuchkov@sfu.ca>
2019-02-25 11:17:34 -0800

    Quantization header

---
+++ include/Quantization.h
@@ -0,0 +1,13 @@
+//
+//  Quantization.h
+//  Compression
+//
+//  Created by admin on 2019-02-25.
+//  Copyright © 2019 maximpuchkov. All rights reserved.
+//
+
+#ifndef Quantization_h
+#define Quantization_h
+
+
+#endif /* Quantization_h */
