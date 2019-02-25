f73dffe7a51713f22fea76d3b2e7e2263a9fd9d6
Maxim Puchkov <mpuchkov@sfu.ca>
2019-02-25 11:17:07 -0800

    Quantization Tables header; values of two tables

---
+++ include/QuantizationTables.h
@@ -0,0 +1,31 @@
+//
+//  QuantizationTables.h
+//  Compression
+//
+//  Created by admin on 2019-02-25.
+//  Copyright © 2019 maximpuchkov. All rights reserved.
+//
+
+#ifndef QuantizationTables_h
+#define QuantizationTables_h
+
+#include <opencv2/opencv.hpp>
+
+// Luminance
+
+// 16, 11, 10, 16, 24, 40, 51, 61, 12, 12, 14, 19, 26, 58, 60, 55, 14, 13, 16, 24, 40, 57, 69, 56, 14, 17, 22, 29, 51, 87, 80, 62, 18, 22, 37, 56, 68, 109, 103, 77, 24, 35, 55, 64, 81, 104, 113, 92, 49, 64, 78, 87, 103, 121, 120, 101, 72, 92, 95, 98, 112, 100, 103, 99
+
+
+
+
+// Chrominance
+
+// 17, 18, 24, 47, 99, 99, 99, 99, 18, 21, 26, 66, 99, 99, 99, 99, 24, 26, 56, 99, 99, 99, 99, 99, 47, 66, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99
+
+// https://csil-git1.cs.surrey.sfu.ca/A2-365/JPEG-Image-Compression/blob/master/README.md#resources-3
+
+
+
+
+
+#endif /* QuantizationTables_h */
